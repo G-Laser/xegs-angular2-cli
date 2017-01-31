bc1968f3375a60c117ab901a95e96af74c929e4a
G-Laser <M_S_Glaser@hotmail.com>
2017-01-31 05:59:43 -0800

    image bug fixes

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,15 +10,15 @@ import {Podcast} from './podcast';
 
 export class SidebarComponent  {
     PODCASTS = [
-    new Podcast('1632 Atari podcaST', 'https://1632podcast.info/', '../../assets/img/podcasts/1632podcast.jpg'),
-    new Podcast('The 2600 Game by Game Podcast', 'http://2600gamebygamepodcast.libsyn.com/', '../../assets/img/podcasts/2600CbC.jpg'),
-    new Podcast('The 5200 Super Podcast', 'http://the5200superpodcast.blogspot.com/', '../../assets/img/podcasts/super5200.jpg'),
-    new Podcast('The 7800 Game by Game Podcast', 'http://atari7800gamebygamepodcast.blogspot.com/', '../../assets/img/podcasts/NoSwearGamer.jpg'),
-    new Podcast('The Atari Antic Podcast', 'http://ataripodcast.libsyn.com/', '../../assets/img/podcasts/Antic.png'),
-    new Podcast('The Atari Jaguar Game by Game Podcast', 'http://atarijaguargamebygame.libsyn.com/', '../../assets/img/podcasts/JaguarPodcast.jpg'),
-    new Podcast('The Atari Lynx Game by Game podcast', 'http://atarilynxpodcast.blogspot.com/', '../../assets/img/podcasts/LynxShow.jpg'),
-    new Podcast('InverseAtascii Atari 8-bit Productivity Podcast', 'http://inverseatascii.info/', '../../assets/img/podcasts/InverseAtasciiCart.jpg'),
-    new Podcast('The Player/Missile Podcast', 'http://playermissile.com/', '../../assets/img/podcasts/PM_logo.png')
+    new Podcast('1632 Atari podcaST', 'https://1632podcast.info/', '1632podcast.jpg'),
+    new Podcast('The 2600 Game by Game Podcast', 'http://2600gamebygamepodcast.libsyn.com/', 'img/podcasts/2600CbC.jpg'),
+    new Podcast('The 5200 Super Podcast', 'http://the5200superpodcast.blogspot.com/', './assets/img/podcasts/super5200.jpg'),
+    new Podcast('The 7800 Game by Game Podcast', 'http://atari7800gamebygamepodcast.blogspot.com/', 'assets/img/podcasts/NoSwearGamer.jpg'),
+    new Podcast('The Atari Antic Podcast', 'http://ataripodcast.libsyn.com/', 'assets/img/podcasts/Antic.png'),
+    new Podcast('The Atari Jaguar Game by Game Podcast', 'http://atarijaguargamebygame.libsyn.com/', 'assets/img/podcasts/JaguarPodcast.jpg'),
+    new Podcast('The Atari Lynx Game by Game podcast', 'http://atarilynxpodcast.blogspot.com/', 'assets/img/podcasts/LynxShow.jpg'),
+    new Podcast('InverseAtascii Atari 8-bit Productivity Podcast', 'http://inverseatascii.info/', 'assets/img/podcasts/InverseAtasciiCart.jpg'),
+    new Podcast('The Player/Missile Podcast', 'http://playermissile.com/', 'assets/img/podcasts/PM_logo.png')
     ];
 relatedPodcasts = this.PODCASTS[0];
 }
